02255153f004aece83325fc0b0163620a1b292e4
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-10 21:21:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php 
-echo "Working...";
-echo "Testing..."
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Project</title>
+  </head>
+
+<body>
+
+<?php
+echo "Deploy test.";
 ?>
+
+</body>
+
+</html>
