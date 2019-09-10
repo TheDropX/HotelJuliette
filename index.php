126010be0b4814db4f21845fb44edf80ec5d3352
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-10 23:55:49 +0300

    :sparkles: Major update.

--- index.php
+++ index.php
@@ -1,44 +1,46 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Project</title>
-  </head>
+<!DOCTYPE html>
+<html>
 
-<body>
-    <style>
-    ::-webkit-scrollbar { 
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>Firma de exercitiu</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/navbar1.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
 
-        display: none; 
+<body style="background: #121212">
+    <nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark navbar--apple">
+        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#menu"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"><i class="la la-navicon"></i></span></button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="nav navbar-nav flex-grow-1 justify-content-between">
+                    <li class="nav-item d-none d-xs-block d-md-block" role="presentation"><a class="nav-link" href="#"><i class="fa fa-cog fa-lg"></i></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Echipa</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Rezervari</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Contact</a></li>
+                    <li class="nav-item d-none d-xs-block d-md-block" role="presentation"><a class="nav-link" href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-    }
-
-    body {
-
-        background-color: #121212;
-
-    }
-
-    .first {
-
-        width: 50%;
-        height: 50%;
-
-    }
-
-    .first_img {
-
-        background-image: url("./img/first.jpg");
-        filter: blur(5px) brightness(50%);
-
-    }
-    </style>
-
-    <div class="first">
-        <img src="./img/first.jpg" class="first_img">
+    <div class="bg"></div>
+    <div class="first_hm">
+            <h1 class="first_h1" data-aos="zoom-in">Firma de exercitiu</h1>
+            <p class="first_p">In development...</p>
     </div>
-
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/navbar.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+            AOS.init();
+    </script>
 </body>
 
 </html>
