3ecbd2113fd24e855a89d20d7df6a5fb6d6e61d0
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-11 17:28:17 +0300

    Updated.

---
+++ sidebar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark navbar--apple">
+        <div class="container">
+            <button data-toggle="collapse" class="navbar-toggler" data-target="#menu">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon">
+                    <i class="la la-navicon"></i>
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="nav navbar-nav flex-grow-1 justify-content-between">
+                    <li class="nav-item d-none d-xs-block d-md-block" role="presentation">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-cog fa-lg"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="/echipa.php">Echipa</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="/rezervari.php">Rezervari</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="/contact.php">Contact</a>
+                    </li>
+                    <li class="nav-item d-none d-xs-block d-md-block" role="presentation">
+                        <a class="nav-link" href="#"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</nav>
