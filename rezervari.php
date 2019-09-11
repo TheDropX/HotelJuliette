d4df2e4d6e104f0b6ec78d86cb408da3c3b262c5
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-11 17:20:31 +0300

    Update rezervari.php

--- rezervari.php
+++ rezervari.php
@@ -1,75 +1,38 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-        <title>Firma de exercitiu</title>
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-        <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
-        <link rel="stylesheet" href="assets/css/navbar1.css">
-        <link rel="stylesheet" href="assets/css/navbar.css">
-        <link rel="stylesheet" href="assets/css/styles.css">
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    </head>
 
-    <body>
-    <style>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>Firma de exercitiu</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/navbar1.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
 
-    body, html {
-    height: 100%;
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-    color: white;
-}
+<body style="background: #121212">
+    <nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark navbar--apple">
+        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#menu"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"><i class="la la-navicon"></i></span></button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="nav navbar-nav flex-grow-1 justify-content-between">
+                    <li class="nav-item d-none d-xs-block d-md-block" role="presentation"><a class="nav-link" href="#"><i class="fa fa-cog fa-lg"></i></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Echipa</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="rezervari.php">Rezervari</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Contact</a></li>
+                    <li class="nav-item d-none d-xs-block d-md-block" role="presentation"><a class="nav-link" href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-    .bgimg {
-    background-image: url("../img/first.jpg");
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    filter: blur(3px) brightness(75%) contrast(100%) invert(0%) saturate(100%);
-    color: white;
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-    font-size: 25px;
-    }
-
-    .topleft {
-    position: absolute;
-    top: 0;
-    left: 16px;
-    }
-
-    .bottomleft {
-    position: absolute;
-    bottom: 0;
-    left: 16px;
-    }
-
-    .middle {
-    position: absolute;
-    color: white;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-    }
-
-    hr {
-    margin: auto;
-    width: 40%;
-    }
-    </style>
-    <div class="bgimg"></div>
-    <div class="topleft">
-        <p>hotel.thedrop.me</p>
-    </div>
-    <div class="middle">
-        <h1>In development...</h1>
-        <hr>
-        <p>7 days</p>
-    </div>
-    <div class="bottomleft">
-        <p>School project.</p>
+    <div class="bg"></div>
+    <div class="first_hm">
+            <h1 class="first_h1" data-aos="zoom-in">Firma de exercitiu</h1>
+            <p class="first_p">In development...</p>
     </div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
@@ -78,5 +41,6 @@
     <script>
             AOS.init();
     </script>
-    </body>
+</body>
+
 </html>
