7e45fda1c328f5c46652c10baaa7239e753d3aa8
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-12 22:39:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -32,10 +32,11 @@ include_once "sidebar.php";
     <div class="container">
         <div class="roombg"></div>
 
+        <div class="first_hm">
+            <h1 class="first_h1" data-aos="zoom-in">Test</h1>
+            <p class="first_p">Test</p>
     </div>
-    <div class="first_hm">
-            <h1 class="first_h1" data-aos="zoom-in">Firmă de exercițiu</h1>
-            <p class="first_p">Pagină în construcție...</p>
+
     </div>
 
     <script src="assets/js/jquery.min.js"></script>
