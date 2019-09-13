8765dec145331279ac695c627748fd3456aa6323
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-13 20:41:44 +0300

    test

--- index.php
+++ index.php
@@ -41,13 +41,13 @@ include_once "sidebar.php";
     <section style="background: #fff;">
     <div class="container" style="text-align: center; padding: 30px 20px;">
         <h1 style="color: #8e4905;">Hotel x</h1>
-        <p>
-Located in the city center of Iasi, Romania, part of the newest modern complex in Moldova – Palas Iasi – the 4-star Hotel International Iasi is the most elegant business, leisure and events location in the Moldavian capital. One of the few hotels in Iasi with views to the Palace of Culture, many consider it to be one of the best hotels in the city center. The hotel is proud to have been recognized for its superior service and hospitality with a number of outstanding awards, including the TripAdvisor Travelers’ Choice Award, Certificate of Excellence and Top Hotel Awards.
+        <p>Situat în centrul orașului, Hotel Internațional Iași, face parte din cel mai modern ansamblu urbanisitic din Moldova – Palas, și este cea mai elegantă locație pentru business, evenimente și relaxare din oraș. Situat în centrul istoric, cu vedere spre Palatului Culturii, hotelul este recunoscut pentru serviciile sale superioare, susținute de numeroasele premii dobândite, precum „TripAdvisor Traveler’s Choice Award” , „Certificate of Excellence” și „Top Hotel Awards”.
 
-With numerous tourist attractions and cultural areas nearby, Hotel International Iasi offers modern, luxurious accommodation with complimentary Wi-Fi and access to wellness and it is featuring an exceptional indoor swimming pool, fitness center, dry and wet saunas, emotional shower and hydro-massage tub. 
-All rooms feature air conditioning, cable flat screen television, minibar, tea and coffee making facilities, a writing desk, a safe deposit box, and a private bathroom with shower or tub. Some rooms offer panoramic views of the city, making it the ideal accommodation for business or a romantic couples retreat.
+Localizarea centrală, proximitatea față de principalele atracții turistice și culturale, serviciile moderne de cazare, cu acces la centrul Spa, sala de fitness, piscina interioară, Jacuzzi-ul, sauna, internetul cât și parcarea gratuită întregesc serviciile complete pe care Hotelul Internațional le oferă. 
+Toate camerele au aer condiționat, televizor LED conectat la canale naționale și internaționale, minibar, facilități de preparare pentru ceai și cafea, birou, seif și o baie dotată cu duș sau cadă. Vederea panoramică asupra orașului cât și toate serviciile oferite, transformând hotelul în locația perfectă pentru o călătorie de afaceri sau o escapadă romantică.
  
-Guests of the Hotel International Iasi can enjoy an exceptional meal at the three elegant a la carte restaurants or may choose to sit back and relax while enjoying glorious views in the hotel’s Sky Lounge. This 4-star hotel in Iasi also boasts eight conference halls for hosting business meetings and social events.</p>
+Oaspeții hotelului se pot bucura de o masă cu specific internațional la unul dintre cele trei restaurante a la carte sau pot alege să se relaxeze în Sofia Sky Lounge situat la etajul 11, unde se pot bucura de o priveliște panoramică asupra orașului, cât și a Palatului Culturii.
+Hotelul dispune și de 8 săli de conferință, printre care și sala Magnum, ce pot găzdui întâlniri de afaceri sau diverse evenimente sociale.</p>
     </div>
     </section>
     
@@ -55,7 +55,7 @@ Guests of the Hotel International Iasi can enjoy an exceptional meal at the thre
         <div class="roombg"></div>
         <div class="bed"></div>
     </section>
-    
+
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/navbar.js"></script>
