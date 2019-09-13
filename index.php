95b4911ab2757a7f89818d4b7cf52d71b90ba080
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-13 20:36:42 +0300

    testing

--- index.php
+++ index.php
@@ -41,7 +41,13 @@ include_once "sidebar.php";
     <section style="background: #fff;">
     <div class="container" style="text-align: center; padding: 30px 20px;">
         <h1 style="color: #8e4905;">Hotel x</h1>
-        <p>Descriere.</p>
+        <p>
+Located in the city center of Iasi, Romania, part of the newest modern complex in Moldova – Palas Iasi – the 4-star Hotel International Iasi is the most elegant business, leisure and events location in the Moldavian capital. One of the few hotels in Iasi with views to the Palace of Culture, many consider it to be one of the best hotels in the city center. The hotel is proud to have been recognized for its superior service and hospitality with a number of outstanding awards, including the TripAdvisor Travelers’ Choice Award, Certificate of Excellence and Top Hotel Awards.
+
+With numerous tourist attractions and cultural areas nearby, Hotel International Iasi offers modern, luxurious accommodation with complimentary Wi-Fi and access to wellness and it is featuring an exceptional indoor swimming pool, fitness center, dry and wet saunas, emotional shower and hydro-massage tub. 
+All rooms feature air conditioning, cable flat screen television, minibar, tea and coffee making facilities, a writing desk, a safe deposit box, and a private bathroom with shower or tub. Some rooms offer panoramic views of the city, making it the ideal accommodation for business or a romantic couples retreat.
+ 
+Guests of the Hotel International Iasi can enjoy an exceptional meal at the three elegant a la carte restaurants or may choose to sit back and relax while enjoying glorious views in the hotel’s Sky Lounge. This 4-star hotel in Iasi also boasts eight conference halls for hosting business meetings and social events.</p>
     </div>
     </section>
 
