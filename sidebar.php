9707b5eec6cf42006b6920de8fa5f756da89b68e
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-15 17:12:21 +0300

    Updated.

--- sidebar.php
+++ sidebar.php
@@ -1,16 +1,3 @@
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-    <title>Firma de exercitiu</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/navbar1.css">
-    <link rel="stylesheet" href="assets/css/navbar.css">
-    <link rel="stylesheet" href="assets/css/styles.css?version=28">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-</head>
-
 <nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark navbar--apple">
         <div class="container">
             <button data-toggle="collapse" class="navbar-toggler" data-target="#menu">
