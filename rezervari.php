5d2c927be66f20bbae0fe8cf45d0d7a46f4a30a5
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-15 17:15:52 +0300

    Update rezervari.php

--- rezervari.php
+++ rezervari.php
@@ -4,19 +4,6 @@
 include_once "head.php";
 ?>
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-    <title>Firma de exercitiu</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/navbar1.css">
-    <link rel="stylesheet" href="assets/css/navbar.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-</head>
-
 <body style="background: #121212">
 
     <div class="bg"></div>
