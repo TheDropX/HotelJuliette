3425905721c42fba4520e4f07235ff83fcef098c
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-26 23:24:00 +0300

    Update rezervari.php

--- rezervari.php
+++ rezervari.php
@@ -37,6 +37,25 @@ include_once "head.php";
     <h1 class="text-center position-relative" style="padding: 20px; color:white">In construction...</h1>    
 </div>
 
+<div class="room">
+          <img src="http://www.langhamhotels.com/cdn-4fdea42f/globalassets/lhr/tl-chicago/rooms/tlchi-rooms-classic_room-doublebed-1680-945.jpg" style="width: 400px;">
+          <div class="room_type">PREMIUM KING</div>
+          <div class="info">
+            <p>Size: 999mp</p>
+            <p>Size: 999mp</p>
+            <p>Size: 999mp</p>
+            <p>Size: 999mp</p>
+          </div>  
+          <div class="button" id="button-3">
+              <div id="circle"></div>
+              <a href="#"><i class="fas fa-info-circle"></i>&nbsp;Afla mai multe</a>
+          </div>
+          <div class="button" id="button-3">
+              <div id="circle"></div>
+              <a href="#"><i class="fas fa-suitcase-rolling"></i>&nbsp;Rezervare</a>
+          </div>
+        </div> 
+
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/navbar.js"></script>
