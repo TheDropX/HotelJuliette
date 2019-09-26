94dcafa20fc9e109634208bc66d94c9c7e499c8d
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-26 23:36:47 +0300

    :sparkles: Updated.

--- rezervari.php
+++ rezervari.php
@@ -31,30 +31,34 @@ include_once "head.php";
         </div>
     </div>
 
-
 -->
-<div class="container position-relative" style="top: 15vh;">
-    <h1 class="text-center position-relative" style="padding: 20px; color:white">In construction...</h1>    
-</div>
 
-<div class="room">
-          <img src="http://www.langhamhotels.com/cdn-4fdea42f/globalassets/lhr/tl-chicago/rooms/tlchi-rooms-classic_room-doublebed-1680-945.jpg" style="width: 400px;">
-          <div class="room_type">PREMIUM KING</div>
-          <div class="info">
-            <p>Size: 999mp</p>
-            <p>Size: 999mp</p>
-            <p>Size: 999mp</p>
-            <p>Size: 999mp</p>
-          </div>  
-          <div class="button" id="button-3">
-              <div id="circle"></div>
-              <a href="#"><i class="fas fa-info-circle"></i>&nbsp;Afla mai multe</a>
-          </div>
-          <div class="button" id="button-3">
-              <div id="circle"></div>
-              <a href="#"><i class="fas fa-suitcase-rolling"></i>&nbsp;Rezervare</a>
-          </div>
-        </div> 
+    <div class="rooms">
+
+    <?php for(i=0; i<=12; i++) { ?>
+
+        <div class="room">
+            <img src="http://www.langhamhotels.com/cdn-4fdea42f/globalassets/lhr/tl-chicago/rooms/tlchi-rooms-classic_room-doublebed-1680-945.jpg" style="width: 400px;">
+            <div class="room_type">CAMERA PREMIUM</div>
+            <div class="info">
+            <p>Pret pe noapte: 200 RON</p>
+            <p>Paturi: 3</p>
+            <p>Băi: 2</p>
+            <p>Capacitate: 2x adulti, 3x copii</p>
+            </div>  
+            <div class="button" id="button-3">
+                <div id="circle"></div>
+                <a href="#"><i class="fas fa-info-circle"></i>&nbsp;Afla mai multe</a>
+            </div>
+            <div class="button" id="button-3">
+                <div id="circle"></div>
+                <a href="#"><i class="fas fa-suitcase-rolling"></i>&nbsp;Rezervare</a>
+            </div>
+        </div>
+
+    <?php } ?>
+
+    </div>
 
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
