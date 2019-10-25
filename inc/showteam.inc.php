3ac97508bf0bf90b08ea8d449431656d5db64d96
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-10-25 09:58:54 +0300

    Add files via upload

--- inc/showteam.inc.php
+++ inc/showteam.inc.php
@@ -12,26 +12,26 @@ class showTeam extends Echipa {
             echo '
             <h1 class="text-center position-relative" style="padding: 20px;">' . $data['Role'] . '</h1>
             ';
-
-        }
-
-        foreach($data0 as $data) {
+            
+            foreach($data0 as $data) {
 
             
 
-            echo '
-            <div class="card card-custom mx-auto">
-            <img src="https://i.imgur.com/ePGIcEg.jpg" class="rounded-circle" style="position: absolute; width: 30%; left: 15px;">
-            <div class="teamcardname">
-            ' . $data['Prenume'] . '
-            </div>
-            <div class="teamcardrole">
-            ' . $data['Role'] . '
-            </div>
-            </div>
-            ';
-
-            //echo $data['Prenume'];
+                echo '
+                <div class="card card-custom mx-auto">
+                <img src="https://i.imgur.com/ePGIcEg.jpg" class="rounded-circle" style="position: absolute; width: 30%; left: 15px;">
+                <div class="teamcardname">
+                ' . $data['Prenume'] . '
+                </div>
+                <div class="teamcardrole">
+                ' . $data['Role'] . '
+                </div>
+                </div>
+                ';
+    
+                //echo $data['Prenume'];
+    
+            }
 
         }
 
