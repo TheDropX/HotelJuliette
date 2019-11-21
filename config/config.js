1d0dc15d791d4c6f69e1f460f1a5423e4596eb46
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-11-21 21:43:01 +0200

    :sparkles: Forgot something.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "facebook_api_key"      :     process.env.FACEBOOK_API_KEY,
     "facebook_api_secret"   :     process.env.FACEBOOK_API_SECRET,
-    "callback_url"          :     "https://localhost:2608/auth/facebook/callback",
+    "callback_url"          :     "https://hotel.thedrop.me/auth/facebook/callback",
     "use_database"          :      true,
     "host"                  :     process.env.DB_HOST,
     "username"              :     process.env.DB_USER,
